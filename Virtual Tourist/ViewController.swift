e833efa25691144be958223f9dc1bc2feba2cff2
Amal Alqadhibi <amalqudaibi@gmail.com>
2019-06-16 02:46:41 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Amal Alqadhibi on 16/06/2019.
+//  Copyright © 2019 Amal Alqadhibi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
